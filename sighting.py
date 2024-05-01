adc8e56a45e8bc687d3fae61e7a8b57be4762823
Zhengxu SUN <zhengxu.sun@cqumail.com>
2024-05-01 17:44:24 +1000

    display menu

---
+++ sighting.py
@@ -0,0 +1,14 @@
+def display_menu():
+    print("Help:")
+    print("=====")
+    print("Display  help                   wildlife> help")
+    print("Exit the application            wildlife> exit")
+
+
+def main():
+    while True:
+        command = input("wildlife> ")
+        if command == "help":
+            display_menu()
+        elif command == "exit":
+            break
