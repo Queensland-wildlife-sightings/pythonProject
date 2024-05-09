d59c7e4f8e3fd5ef47b3f27b69fa466d7052bb83
Zhengxu SUN <zhengxu.sun@cqumail.com>
2024-05-09 20:32:41 +1000

    task3

--- sighting.py
+++ sighting.py
@@ -13,31 +13,30 @@ def venomous_display():
 
 
 def filter_venomous(species_list):
-    #fiter to choice the Venomous
-    return [species for species in species_list 
+    # fiter to choice the Venomous
+    return [species for species in species_list
             if species['Species']['PestStatus'] == 'Venomous']
 
 
 def display_sightings(sightings):
     # print the list of animal sightings 
-    
+
     for sighting in sightings:
-        print(f"Start Date: {sighting['properties']['StartDate']}, Locality: {sighting['properties']['LocalityDetails']}")
+        print(
+            f"Start Date: {sighting['properties']['StartDate']}, Locality: {sighting['properties']['LocalityDetails']}")
 
 
 def display_species(species_list):
-    #display each species in the list
-    for species in species_list:
-        pass
-     
+    # display_species(species_list) that prints a list of species to the screen
+    for spec in species_list:
+        print(
+            f"Accepted Common Name: {spec['Species']['AcceptedCommonName']},Pest Status: {spec['Species']['PestStatus']}")
+
 
 def main():
-    
-
     while True:
         command = input("wildlife> ")
         input_commands = command.split(" ")
-        
 
         # if len(input_commands) == 0 and command =="help" :
         if command == "help":
@@ -47,21 +46,18 @@ def main():
         #     break
         elif input_commands[0] == "species":
             if len(input_commands) > 1:
-                city  = input_commands[1]
-                
+                city = input_commands[1]
+
                 spc_list = search_species(city)
                 display_species(spc_list)
 
-     
 
 
-        
+
+
         # Update main() to accept the command “sightings” followed by a species, a comma and an
         # area. When this command is received, use the search_sightings(species,area) and
         # display_sightings(sightings) functions to display a list of sightings to the user.
-            
-
-
 
         elif command == "exit":
             break
@@ -70,15 +66,17 @@ def main():
 def search_sightings(species, area):
     return [{"properties": {"StartDate": "1999-11-15", "LocalityDetails": "Tinaroo"}}]
 
+
 def search_species(city):
     return [
-        {"Species":{"AcceptedCommonName":"dolphin", "PestStatus":"Nil"}},
-        {"Species":{"AcceptedCommonName":"snake","PestStatus":"Venomous"}}
+        {"Species": {"AcceptedCommonName": "dolphin", "PestStatus": "Nil"}},
+        {"Species": {"AcceptedCommonName": "snake", "PestStatus": "Venomous"}}
     ]
+
 
 def sear_species(taxonid, city):
     #  return a fixed list of animal sightings
-    return [{"properties": {"StartDate": "1999-11-15", 
+    return [{"properties": {"StartDate": "1999-11-15",
                             "LocalityDetails": "Tinaroo"}}]
 
 
